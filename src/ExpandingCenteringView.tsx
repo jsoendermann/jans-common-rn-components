258c118c62491eadd373d6b5e96edcbe7ff7605c
Jan Soendermann <jan.soendermann+git@gmail.com>
2017-06-20 10:50:06 +0800

    Add declarations

--- src/ExpandingCenteringView.tsx
+++ src/ExpandingCenteringView.tsx
@@ -1,4 +1,7 @@
+import { StyledComponentClass } from 'styled-components'
 import styled from 'styled-components/native'
+import { ViewProperties } from 'react-native'
+
 
 export const ExpandingCenteringView = styled.View`
   flex: 1;
