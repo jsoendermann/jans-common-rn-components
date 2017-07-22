b56d9ea144b0fb23876b386e752d581e99159e51
Jan Soendermann <jan.soendermann+git@gmail.com>
2017-07-22 19:02:39 +0800

    Add export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './ExpandingView'
 export * from './ExpandingCenteringView'
 // export * from './NavigationHeaderButtons'
+export * from './ConstructStackNavigatorWithDefaultNavigationOptions'
