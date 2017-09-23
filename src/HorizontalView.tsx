875a75b641d44ca3772c730c7b3ed16b8d17b022
Jan Soendermann <jan.soendermann+git@gmail.com>
2017-09-23 16:46:41 +0800

    :sparkles: HorizontalView

---
+++ src/HorizontalView.tsx
@@ -0,0 +1,5 @@
+import { ViewProperties } from 'react-native'
+import { StyledComponentClass } from 'styled-components'
+import styled from 'styled-components/native'
+
+export const HorizontalView = styled.View`flex-direction: row;`
