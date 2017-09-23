d854842413410043b22854e142618475807d158b
Jan Soendermann <jan.soendermann+git@gmail.com>
2017-09-23 17:00:54 +0800

    :sparkles: AbsoluteFillView

---
+++ src/AbsoluteFillView.tsx
@@ -0,0 +1,21 @@
+import { View, ViewStyle, StyleSheet } from 'react-native'
+
+export interface AbsoluteFillViewProps {
+  style?: ViewStyle
+  children?: any
+}
+export const AbsoluteFillView = ({
+  style,
+  children,
+}: AbsoluteFillViewProps) => (
+  <View
+    style={{
+      ...StyleSheet.absoluteFillObject,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'transparent',
+    }}
+  >
+    {children}
+  </View>
+)
